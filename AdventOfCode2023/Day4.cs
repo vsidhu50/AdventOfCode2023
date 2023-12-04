af62b6202f34f0bf34342b22910fcfb41a531b77
vsidhu50 <vsidhu50@gmail.com>
2023-12-04 10:38:50 -0500

    day4

---
+++ AdventOfCode2023/Day4.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode2023;
+
+public class Day4
+{
+    public Day4()
+    {
+    }
+
+    public static int PartOne()
+    {
+        var lines = Utilities.GetInputLines(4);
+        var score = 0;
+        foreach (var line in lines)
+        {
+            var nums = line.Split(":")[1];
+            var game = nums.Split("|");
+            var winning = game[0].Trim().Split(" ").Where(x => x != "").Select(int.Parse);
+            var have = game[1].Trim().Split(" ").Where(x => x != "").Select(int.Parse);
+
+            var contains = 0;
+            foreach (var num in winning)
+            {
+                if (have.Contains(num))
+                    contains++;
+            }
+            if (contains > 0)
+                score += (int) Math.Pow(2, contains - 1);
+        }
+        return score;
+    }
+
+    public static int PartTwo()
+    {
+        var lines = Utilities.GetInputLines(4);
+        var copies = new Dictionary<int, int>();
+
+        for (int i = 1; i <= lines.Count; i++)
+        {
+            copies.Add(i, 1);
+        }
+
+        var gameNum = 1;
+        foreach (var line in lines)
+        {
+            var nums = line.Split(":")[1];
+            var game = nums.Split("|");
+            var winning = game[0].Trim().Split(" ").Where(x => x != "").Select(int.Parse);
+            var have = game[1].Trim().Split(" ").Where(x => x != "").Select(int.Parse);
+
+            var contains = 0;
+            foreach (var num in winning)
+            {
+                if (have.Contains(num))
+                    contains++;
+            }
+            if (contains > 0)
+            {
+                for (int i = 1; i <= contains; i++)
+                {
+                    copies[gameNum + i] += copies[gameNum];
+                }
+            }
+            gameNum++;
+                
+        }
+        return copies.Values.Sum();
+    }
+}
+
